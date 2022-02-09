23963fd2e29d4fdc63d9970f9756cd67488bdc74
liwi <77789571+dliwilb@users.noreply.github.com>
2022-02-09 11:12:19 +0100

    contract info page

--- script/frens-contract.js
+++ script/frens-contract.js
@@ -13,8 +13,23 @@ async function readContractStatVar(){
 
     const nftContract = new ethers.Contract(nftContractAddress, frensAbi, provider);
 
-    const nft_tokenIds = await nftContract._tokenIds();
-    console.log(Number(nft_tokenIds));
+    let listContent = '';
+    // const nft_tokenIds = await nftContract._tokenIds();
+    // console.log(Number(nft_tokenIds));
+
+    listContent += "<li>_tokenIds: " + await nftContract._tokenIds() + "</li>"; 
+    listContent += "<li>isAddUserAllowed: " + await nftContract.isAddUserAllowed() + "</li>";
+    listContent += "<li>mintFee: " + await nftContract.mintFee() + "wei </li>";
+    listContent += "<li>whitelistFee: " + await nftContract.whitelistFee() + "wei </li>";
+    listContent += "<li>whitelistWaiver: " + await nftContract.whitelistWaiver() + "</li>";
+    listContent += "<li>name: " + await nftContract.name() + "</li>";
+    listContent += "<li>symbol: " + await nftContract.symbol() + "</li>";
+    listContent += "<li>nftNameSymbolHardcoded: " + await nftContract.nftNameSymbolHardcoded() + "</li>";
+    listContent += "<li>owner: " + await nftContract.owner() + "</li>";
+    listContent += "<li>treasuryAddress: " + await nftContract.treasuryAddress() + "</li>";
+
+
+    document.getElementById('div-contract-info').innerHTML = listContent;
 
 }
 
