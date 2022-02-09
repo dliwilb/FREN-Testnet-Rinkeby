3d648ca0baac87254400abbe5c2c90d342caff1f
liwi <77789571+dliwilb@users.noreply.github.com>
2022-02-09 16:35:49 +0100

    Update frens-latest.js

--- script/frens-latest.js
+++ script/frens-latest.js
@@ -15,43 +15,54 @@ async function showLatestFrens() {
 
     const eventFilter = nftContract.filters.tokenCreated();
     const events = await nftContract.queryFilter(eventFilter, fromBlock, toBlock);
-    console.log(events);
+    // console.log(events);
 
+    let previousTokenURI = '';
     for (let i = events.length-1; i >= 0; i--) {
         const newItemId = events[i].args[0];
-        let tokenURI = events[i].args[1];
+        let tokenURI = events[i].args[1].trim();
         const createdBy = events[i].args[2];
         
-        const foundIPFSinURI = tokenURI.match(/ipfs:\/\/(\w+)/);
-        if (foundIPFSinURI[1] != ''){
-            tokenURI = 'https://ipfs.io/ipfs/' + foundIPFSinURI[1];
+        // console.log((i+1) + ": " + tokenURI);
+        // console.log(previousTokenURI);
+
+        if (tokenURI !== previousTokenURI) {
+            previousTokenURI = tokenURI;
+
+            const foundIPFSinURI = tokenURI.match(/ipfs:\/\/(\w+)/);
+            if (foundIPFSinURI[1] != ''){
+                tokenURI = 'https://ipfs.io/ipfs/' + foundIPFSinURI[1];
+            }
+
+            let nftJSON = await fetchJSON(tokenURI);
+
+
+            const foundIPFSinJSONImage = nftJSON.image.match(/ipfs:\/\/(\w+)/);
+            if (foundIPFSinJSONImage[1] != ''){
+                nftJSON.image = 'https://ipfs.io/ipfs/' + foundIPFSinJSONImage[1];
+            }
+
+            // console.log(nftJSON.image);
+
+            const createdByShort = createdBy.substring(0, 6) + '...' + createdBy.substring(createdBy.length - 4);
+
+            document.getElementById('div-latest-frens').innerHTML +=
+                `
+                <div class="nft-item">
+                    <img class="nft-image" src="${nftJSON.image}">
+                    <div class="nft-token-info">
+                        fren # ${newItemId}&nbsp;&nbsp;made by&nbsp;&nbsp;${createdByShort}
+                    </div>
+                    <div class="nft-token-info">
+                        ${nftJSON.name}
+                    </div>
+                    <div class="nft-token-info">
+                        ${nftJSON.description}
+                    </div>
+                </div>
+                `;
+
         }
-
-        let nftJSON = await fetchJSON(tokenURI);
-        const foundIPFSinJSONImage = nftJSON.image.match(/ipfs:\/\/(\w+)/);
-        if (foundIPFSinJSONImage[1] != ''){
-            nftJSON.image = 'https://ipfs.io/ipfs/' + foundIPFSinJSONImage[1];
-        }
-
-        // console.log(nftJSON.image);
-
-        const createdByShort = createdBy.substring(0, 6) + '...' + createdBy.substring(createdBy.length - 4);
-
-        document.getElementById('div-latest-frens').innerHTML +=
-            `
-            <div class="nft-item">
-                <img class="nft-image" src="${nftJSON.image}">
-                <div class="nft-token-info">
-                    fren # ${newItemId} made by ${createdByShort}
-                </div>
-                <div class="nft-token-info">
-                    ${nftJSON.name}
-                </div>
-                <div class="nft-token-info">
-                    ${nftJSON.description}
-                </div>
-            </div>
-            `;
     }
 
     // let listContent = '';
