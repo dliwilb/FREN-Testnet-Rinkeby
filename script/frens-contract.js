bcf356a4100781727a3cc8c5e01052e11db59d44
liwi <77789571+dliwilb@users.noreply.github.com>
2022-02-09 11:45:13 +0100

    adduser interface

--- script/frens-contract.js
+++ script/frens-contract.js
@@ -4,13 +4,13 @@ const nftContractAddress = '0x69F511EAca22eD5c5f48ba3d5D3D0442340948c9'; // v2.2
 
 async function readContractStatVar(){
     console.log('Reading contract state variables...');
-    document.getElementById('div-contract-info').innerHTML = 'Loading...';
+    document.getElementById('list-contract-info').innerHTML = 'Loading...';
 
-    await connectWallet();
-    await switchNetwork();
+    // await connectWallet();
+    // await switchNetwork();
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-
+    // const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = new ethers.providers.JsonRpcProvider('https://rinkeby-light.eth.linkpool.io/');
     const nftContract = new ethers.Contract(nftContractAddress, frensAbi, provider);
 
     let listContent = '';
@@ -29,7 +29,54 @@ async function readContractStatVar(){
     listContent += "<li>treasuryAddress: " + await nftContract.treasuryAddress() + "</li>";
 
 
-    document.getElementById('div-contract-info').innerHTML = listContent;
+    document.getElementById('list-contract-info').innerHTML = listContent;
+
+}
+
+async function readWhitelisted() {
+    console.log('Reading whitelisted addresses...');
+    document.getElementById('list-whitelisted').innerHTML = 'Loading...';
+
+    // const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // const provider = new ethers.providers.JsonRpcProvider('https://rinkeby-light.eth.linkpool.io/');
+    const provider = ethers.getDefaultProvider(4);
+    const nftContract = new ethers.Contract(nftContractAddress, frensAbi, provider);
+
+    let listContent = '';
+
+    const eventFilter = nftContract.filters.userAdded();
+    const events = await nftContract.queryFilter(eventFilter);
+
+    for (let i = events.length-1; i >= 0; i--) {
+        const newUser = events[i].args[1];
+        const approvedBy = events[i].args[1];
+        // console.log(newUser + " added by " + approvedBy);
+        listContent += `<li>${newUser} (added by ${approvedBy})</li>`;
+    }
+
+    document.getElementById('list-whitelisted').innerHTML = listContent;
+
+
+    // console.log("events: ");
+    // console.log(events);
+
+}
+
+async function addUserWhitelistWaived() {
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const nftContract = new ethers.Contract(nftContractAddress, frensAbi, signer);
+    
+    let whitelistFee = await nftContract.whitelistFee();
+    whitelistFee = Number(whitelistFee);
+    console.log(whitelistFee);
+    console.log(document.getElementById('address-to-be-whitelisted').value);
+
+    await nftContract.addUserWhitelistWaived(
+        document.getElementById('address-to-be-whitelisted').value, 
+        { value: whitelistFee }
+    ); 
 
 }
 
